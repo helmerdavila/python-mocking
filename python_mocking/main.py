b3720a1cd89413a702d048f32836548fed8c3a2a
Helmer <11061182+helmerdavila@users.noreply.github.com>
2023-07-04 01:31:14 +0000

    Installing black as a formatter

--- python_mocking/main.py
+++ python_mocking/main.py
@@ -2,5 +2,5 @@ import requests
 
 
 def hello_from_docker():
-    request = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+    request = requests.get("https://jsonplaceholder.typicode.com/todos/1")
     print(request.json())
