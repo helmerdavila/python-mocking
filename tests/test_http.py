2c3c06e104cdbed412e4927a898407d93c17ddfb
Helmer <11061182+helmerdavila@users.noreply.github.com>
2023-08-06 02:56:07 +0000

    Using decorators for patching

--- tests/test_http.py
+++ tests/test_http.py
@@ -1,27 +1,26 @@
+from unittest.mock import patch
 from python_mocking import call_single_endpoint
 
 
-def test_call_single_endpoint_mock(mocker):
-    mocked_http_client = mocker.patch("requests.get")
+@patch("requests.get")
+def test_call_single_endpoint_mock(mocked_http_client):
     call_single_endpoint()
     assert mocked_http_client.called_once
 
 
-def test_call_and_stubbed_response(mocker):
-    mocked_http_client = mocker.patch("requests.get")
+@patch("requests.get")
+def test_call_and_stubbed_response(mocked_http_client):
     mocked_http_client.return_value.json.return_value = {"foo": "bar"}
     call_single_endpoint()
-    assert mocked_http_client.called
-    assert mocked_http_client.return_value.json.call_count == 1
+    assert mocked_http_client.called_once
     assert mocked_http_client.return_value.json.return_value == {"foo": "bar"}
 
 
-def test_call_and_response_different_values(mocker):
-    mocked_http_client = mocker.patch("requests.get")
+@patch("requests.get")
+def test_call_and_response_different_values(mocked_http_client):
     mocked_http_client.return_value.json.side_effect = [
         {"fruit": "orange"}, {"fruit": "apple"}]
     first_result = call_single_endpoint()
-    assert mocked_http_client.called
     assert mocked_http_client.return_value.json.call_count == 1
     assert first_result == {"fruit": "orange"}
     second_result = call_single_endpoint()
