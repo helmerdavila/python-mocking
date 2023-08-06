f87cd817654e3a4e5bce0f46b51add6a7c87cee4
Helmer <11061182+helmerdavila@users.noreply.github.com>
2023-08-06 02:50:31 +0000

    Creating a test with side effects

--- python_mocking/main.py
+++ python_mocking/main.py
@@ -3,4 +3,4 @@ import requests
 
 def call_single_endpoint():
     request = requests.get("https://jsonplaceholder.typicode.com/todos/1")
-    print(request.json())
+    return request.json()
--- tests/test_http.py
+++ tests/test_http.py
@@ -4,7 +4,7 @@ from python_mocking import call_single_endpoint
 def test_call_single_endpoint_mock(mocker):
     mocked_http_client = mocker.patch("requests.get")
     call_single_endpoint()
-    assert mocked_http_client.called
+    assert mocked_http_client.called_once
 
 
 def test_call_and_stubbed_response(mocker):
@@ -12,5 +12,18 @@ def test_call_and_stubbed_response(mocker):
     mocked_http_client.return_value.json.return_value = {"foo": "bar"}
     call_single_endpoint()
     assert mocked_http_client.called
-    assert mocked_http_client.return_value.json.called
+    assert mocked_http_client.return_value.json.call_count == 1
     assert mocked_http_client.return_value.json.return_value == {"foo": "bar"}
+
+
+def test_call_and_response_different_values(mocker):
+    mocked_http_client = mocker.patch("requests.get")
+    mocked_http_client.return_value.json.side_effect = [
+        {"fruit": "orange"}, {"fruit": "apple"}]
+    first_result = call_single_endpoint()
+    assert mocked_http_client.called
+    assert mocked_http_client.return_value.json.call_count == 1
+    assert first_result == {"fruit": "orange"}
+    second_result = call_single_endpoint()
+    assert second_result == {"fruit": "apple"}
+    assert mocked_http_client.return_value.json.call_count == 2
