873d0c345ba8a7ad0f7f5caeca25d96881f559d4
Helmer <11061182+helmerdavila@users.noreply.github.com>
2023-08-06 00:30:33 +0000

    Testing with a stubbed response

--- tests/test_http.py
+++ tests/test_http.py
@@ -1,4 +1,3 @@
-import pytest
 from python_mocking import call_single_endpoint
 
 
@@ -6,3 +5,12 @@ def test_call_single_endpoint_mock(mocker):
     mocked_http_client = mocker.patch("requests.get")
     call_single_endpoint()
     assert mocked_http_client.called
+
+
+def test_call_and_stubbed_response(mocker):
+    mocked_http_client = mocker.patch("requests.get")
+    mocked_http_client.return_value.json.return_value = {"foo": "bar"}
+    call_single_endpoint()
+    assert mocked_http_client.called
+    assert mocked_http_client.return_value.json.called
+    assert mocked_http_client.return_value.json.return_value == {"foo": "bar"}
