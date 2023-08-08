56696e9b588adc72999351a752d1c21c546c2d53
Helmer <11061182+helmerdavila@users.noreply.github.com>
2023-08-08 00:43:14 -0400

    Renaming http client by requests get

--- tests/test_http.py
+++ tests/test_http.py
@@ -3,27 +3,28 @@ from python_mocking import call_single_endpoint
 
 
 @patch("requests.get")
-def test_call_single_endpoint_mock(mocked_http_client):
+def test_call_single_endpoint_mock(mocked_requests_get):
     call_single_endpoint()
-    assert mocked_http_client.called_once
+    assert mocked_requests_get.called_once
 
 
 class TestRequests:
     @patch("requests.get")
-    def test_call_and_stubbed_response(self, mocked_http_client):
-        mocked_http_client.return_value.json.return_value = {"foo": "bar"}
+    def test_call_and_stubbed_response(self, mocked_requests_get):
+        mocked_requests_get.return_value.json.return_value = {"foo": "bar"}
         call_single_endpoint()
-        assert mocked_http_client.called_once
-        assert mocked_http_client.return_value.json.return_value == {"foo": "bar"}
-
+        assert mocked_requests_get.called_once
+        assert mocked_requests_get.return_value.json.return_value == {"foo": "bar"}
 
     @patch("requests.get")
-    def test_call_and_response_different_values(self, mocked_http_client):
-        mocked_http_client.return_value.json.side_effect = [
-            {"fruit": "orange"}, {"fruit": "apple"}]
+    def test_call_and_response_different_values(self, mocked_requests_get):
+        mocked_requests_get.return_value.json.side_effect = [
+            {"fruit": "orange"},
+            {"fruit": "apple"},
+        ]
         first_result = call_single_endpoint()
-        assert mocked_http_client.return_value.json.call_count == 1
+        assert mocked_requests_get.return_value.json.call_count == 1
         assert first_result == {"fruit": "orange"}
         second_result = call_single_endpoint()
         assert second_result == {"fruit": "apple"}
-        assert mocked_http_client.return_value.json.call_count == 2
+        assert mocked_requests_get.return_value.json.call_count == 2
