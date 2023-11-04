b4aa942625bc2cd4f4ffcd7aaa6a7868c09196fd
Helmer <11061182+helmerdavila@users.noreply.github.com>
2023-11-03 23:42:29 -0400

    Adding more tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,17 @@
-from .test_http import TestRequests, test_call_single_endpoint_mock, test_call_single_endpoint_mock_without_decorator, test_call_single_endpoint_mock_with_mocker_param
+from .test_http import (
+    test_call_single_endpoint_mock,
+    test_call_single_endpoint_mock_without_decorator,
+    test_call_single_endpoint_mock_with_mocker_param,
+    test_assert_called_with,
+    test_assert_called_once_with,
+    TestRequests,
+)
 
-__all__ = ["TestRequests", "test_call_single_endpoint_mock",
-           "test_call_single_endpoint_mock_without_decorator", "test_call_single_endpoint_mock_with_mocker_param"]
+__all__ = [
+    "TestRequests",
+    "test_call_single_endpoint_mock",
+    "test_call_single_endpoint_mock_without_decorator",
+    "test_call_single_endpoint_mock_with_mocker_param",
+    "test_assert_called_with",
+    "test_assert_called_once_with",
+]
--- tests/test_http.py
+++ tests/test_http.py
@@ -1,5 +1,11 @@
-from unittest.mock import patch
-from python_mocking import call_single_endpoint, call_post_endpoint, call_post_endpoint_with_data
+from unittest.mock import patch, call
+from python_mocking import (
+    call_single_endpoint,
+    call_post_endpoint,
+    call_post_endpoint_with_data,
+    posts_url,
+    todos_single_url,
+)
 
 
 @patch("requests.get")
@@ -20,14 +26,32 @@ def test_call_single_endpoint_mock_with_mocker_param(mocker):
     assert mocked_requests_get.called_once
 
 
+@patch("requests.post")
+def test_assert_called_with(mocked_requests_post):
+    data = {"userId": 3}
+    call_post_endpoint_with_data(data)
+    mocked_requests_post.assert_called_with(posts_url, data)
+
+
+@patch("requests.post")
+def test_assert_called_once_with(mocked_requests_post):
+    data = {"userId": 3}
+    call_post_endpoint_with_data(data)
+    mocked_requests_post.assert_called_once_with(posts_url, data)
+
+
 class TestRequests:
     @patch("requests.get")
     def test_call_and_stubbed_response(self, mocked_requests_get):
         mocked_requests_get.return_value.json.return_value = {"foo": "bar"}
         call_single_endpoint()
-        assert mocked_requests_get.called_once
-        assert mocked_requests_get.return_value.json.return_value == {
-            "foo": "bar"}
+        assert mocked_requests_get.called_once_with(todos_single_url)
+        assert mocked_requests_get.return_value.json.return_value == {"foo": "bar"}
+
+    @patch("requests.get")
+    def test_call_args(self, mocked_requests_get):
+        call_single_endpoint()
+        assert mocked_requests_get.call_args == call(todos_single_url)
 
     @patch("requests.get")
     def test_call_and_response_different_values(self, mocked_requests_get):
@@ -47,8 +71,7 @@ class TestRequests:
         mocked_requests_post.return_value.json.return_value = {"foo": "bar"}
         call_post_endpoint()
         assert mocked_requests_post.called_once
-        assert mocked_requests_post.return_value.json.return_value == {
-            "foo": "bar"}
+        assert mocked_requests_post.return_value.json.return_value == {"foo": "bar"}
 
     @patch("requests.post")
     def test_call_post_endpoint_with_data(self, mocked_requests_post):
