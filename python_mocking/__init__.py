6a9fb1a2d806158ed95d30fb163204a79adbf106
Helmer <11061182+helmerdavila@users.noreply.github.com>
2023-11-03 23:42:17 -0400

    Exporting URL as variable

--- python_mocking/__init__.py
+++ python_mocking/__init__.py
@@ -1,4 +1,15 @@
-from .main import call_single_endpoint, call_post_endpoint, call_post_endpoint_with_data
+from .main import (
+    call_single_endpoint,
+    call_post_endpoint,
+    call_post_endpoint_with_data,
+    posts_url,
+    todos_single_url,
+)
 
-__all__ = ["call_single_endpoint", "call_post_endpoint",
-           "call_post_endpoint_with_data"]
+__all__ = [
+    "call_single_endpoint",
+    "call_post_endpoint",
+    "call_post_endpoint_with_data",
+    "posts_url",
+    "todos_single_url",
+]
--- python_mocking/main.py
+++ python_mocking/main.py
@@ -1,16 +1,19 @@
 import requests
 
+todos_single_url = "https://jsonplaceholder.typicode.com/todos/1"
+posts_url = "https://jsonplaceholder.typicode.com/posts"
+
 
 def call_single_endpoint():
-    request = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    request = requests.get(todos_single_url)
     return request.json()
 
 
 def call_post_endpoint():
-    request = requests.post("https://jsonplaceholder.typicode.com/posts")
+    request = requests.post(posts_url)
     return request.json()
 
 
 def call_post_endpoint_with_data(data):
-    request = requests.post("https://jsonplaceholder.typicode.com/posts", data)
+    request = requests.post(posts_url, data)
     return request.json()
